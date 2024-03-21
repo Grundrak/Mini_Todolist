56e123651c03f198ffe689ec089db6b0275cb3cf
Grundrak <electrovilla66@gmail.com>
2024-03-21 22:47:12 +0000

    fixe some issue

--- Todolist.c
+++ Todolist.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Define a structure to represent a todo item
 typedef struct
 {
     int lvl;
@@ -20,13 +21,13 @@ void add()
     int end = 0;
     printf("Enter ur Task please :");
     fgets(desc, sizeof(desc), stdin);
-    desc[strcspn(desc, "\n")] = 0;
+    desc[strcspn(desc, "\n")] = 0; // Remove newline character from input
 
     do
     {
         printf("Enter your level of priority for this Task from 1 to 3 : ");
         scanf("%d", &lvl);
-        getchar();
+        getchar(); // Clear input buffer
 
         if (lvl < 1 || lvl > 3)
         {
@@ -35,17 +36,16 @@ void add()
     } while (lvl < 1 || lvl > 3);
 
     printf("Enter if you complited the task or not { (1) for done and (0) for unfinished }  : ");
-    scanf("%d",&end);
+    scanf("%d", &end);
     getchar();
 
-
-    todoList = (todo *)realloc(todoList, (count + 1) * sizeof(todo));  
+    todoList = (todo *)realloc(todoList, (count + 1) * sizeof(todo));
 
     todoList[count].id = count + 1;
     strncpy(todoList[count].desc, desc, sizeof(todoList[count].desc) - 1);
     todoList[count].desc[sizeof(todoList[count].desc) - 1] = '\0';
     todoList[count].lvl = lvl;
-    todoList[count].end= end;
+    todoList[count].end = end;
     printf("Added successfully");
     count++;
 }
@@ -54,7 +54,7 @@ void display()
 {
     for (int i = 0; i < count; i++)
     {
-        printf("id: %d | Task : %s  | Lvl Priority : %d  | Task is : %s \n ", todoList[i].id, todoList[i].desc, todoList[i].lvl ,(todoList[i].end == 1) ? "Complited" :"Unfinished");
+        printf("id: %d | Task : %s  | Lvl Priority : %d  | Task is : %s \n ", todoList[i].id, todoList[i].desc, todoList[i].lvl, (todoList[i].end == 1) ? "Complited" : "Unfinished");
     }
 }
 void displayByPriority()
@@ -73,12 +73,14 @@ void displayByPriority()
     }
     for (int i = 0; i < count; i++)
     {
-        printf("id: %d | Task : %s  | Lvl Priority : %d  | Task is : %s \n ", todoList[i].id, todoList[i].desc, todoList[i].lvl ,(todoList[i].end == 1) ? "Complited" :"Unfinished");
+        printf("id: %d | Task : %s  | Lvl Priority : %d  | Task is : %s \n ", todoList[i].id, todoList[i].desc, todoList[i].lvl, (todoList[i].end == 1) ? "Complited" : "Unfinished");
     }
 }
 void update()
 {
     int id;
+    int lvl = 0;
+    int end = 0;
     char newDesc[128];
     printf("Enter the id of task you wanna update : ");
     scanf("%d", &id);
@@ -88,11 +90,29 @@ void update()
     fgets(newDesc, sizeof(newDesc), stdin);
     newDesc[strcspn(newDesc, "\n")] = 0;
 
+    do
+    {
+        printf("Enter your level of priority for this Task from 1 to 3 : ");
+        scanf("%d", &lvl);
+        getchar();
+
+        if (lvl < 1 || lvl > 3)
+        {
+            printf("Error: Number must be from 1 to 3. Please try again.\n");
+        }
+    } while (lvl < 1 || lvl > 3);
+
+    printf("Enter if you complited the task or not { (1) for done and (0) for unfinished }  : ");
+    scanf("%d", &end);
+    getchar();
+
     for (int i = 0; i < count; i++)
     {
         if (todoList[i].id == id)
             strncpy(todoList[i].desc, newDesc, sizeof(todoList[i].desc) - 1);
         todoList[i].desc[sizeof(todoList[i].desc) - 1] = '\0';
+        todoList[count].lvl = lvl;
+        todoList[count].end = end;
         printf("Uptaded successfully");
         break;
     }
@@ -122,13 +142,13 @@ void deleteT()
 int main()
 {
     int choice;
-
     while (1)
     {
-        printf("\n1. Add Task\n2. Display All Tasks\n3. Display Tasks by priority \n4. Update Task \n5. Delete task\n6. Exit\nEnter your choice: ");
-        scanf("%d", &choice);
-        getchar();
 
+            printf("\n1. Add Task\n2. Display All Tasks\n3. Display Tasks by Priority\n4. Update Task\n5. Delete Task\n6. Exit\nEnter your choice: ");
+            scanf("%d", &choice);
+            getchar();
+  
         switch (choice)
         {
         case 1:
@@ -136,9 +156,13 @@ int main()
             break;
         case 2:
             display();
+            printf("Press any key to continue...");
+            getchar(); 
             break;
         case 3:
             displayByPriority();
+            printf("Press any key to continue...");
+            getchar(); 
             break;
         case 4:
             update();
